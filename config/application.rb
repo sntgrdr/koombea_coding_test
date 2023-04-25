1aaafe77f57d4683be4f0fe6d162454c2111e131
sntgrdr <santiago.rodriguez@gogrow.dev>
2023-04-25 14:13:59 -0300

    istalled rubocop gem and run 'rubocop -A'

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
-require_relative "boot"
+# frozen_string_literal: true
 
-require "rails/all"
+require_relative 'boot'
+
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,6 +12,10 @@ module KoombeaCodingTest
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.generators.after_generate do |files|
+      parsable_files = files.filter { |file| file.end_with?('.rb') }
+      system("bundle exec rubocop -A --fail-level=E #{parsable_files.shelljoin}", exception: true)
+    end
 
     # Configuration for the application, engines, and railties goes here.
     #
