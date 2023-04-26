8e4768d81cc5c06e3a92e387334ce8020e7a98e5
sntgrdr <93919358+sntgrdr@users.noreply.github.com>
2023-04-26 19:49:10 -0300

    fixed stimulus problem | added navigation links (#14)

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,9 +5,6 @@
 pin 'application', preload: true
 pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
-pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
-pin_all_from 'app/javascript/controllers', under: 'controllers'
-pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
-pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
+pin '@hotwired/stimulus-rails', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
