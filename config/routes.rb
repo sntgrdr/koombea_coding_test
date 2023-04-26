e3d8c8902ac0e0905a61d002bd9d7246932fdb08
sntgrdr <santiago.rodriguez@gogrow.dev>
2023-04-26 19:48:38 -0300

    fixed stimulus problem | added navigation links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
 
